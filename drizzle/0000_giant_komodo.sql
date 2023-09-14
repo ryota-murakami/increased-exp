42b1a56e2e21e44225a648a7ccf06224b329cb64
ryota-murakami <dojce1048@gmail.com>
2023-09-15 08:06:00 +0900

    up

--- drizzle/0000_giant_komodo.sql
+++ drizzle/0000_giant_komodo.sql
@@ -1,12 +1,3 @@
--- Current sql file was generated after introspecting the database
--- If you want to run this migration please uncomment this code before executing migrations
-/*
-CREATE TABLE `SequelizeMeta` (
-	`name` varchar(255) NOT NULL,
-	CONSTRAINT `SequelizeMeta_name` PRIMARY KEY(`name`),
-	CONSTRAINT `name` UNIQUE(`name`)
-);
---> statement-breakpoint
 CREATE TABLE `authors` (
 	`createdAt` datetime NOT NULL,
 	`id` int AUTO_INCREMENT NOT NULL,
